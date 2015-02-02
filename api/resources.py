4d8db9db136ef724b5eb8555b5d709b36f2fc89f
shouro <sourogts@gmail.com>
2015-02-03 03:19:01 +0600

    adding swagger api doc

--- api/resources.py
+++ api/resources.py
@@ -1,7 +1,44 @@
 # -*- coding: utf-8 -*-
 '''api resources'''
 from flask.ext.restful import Resource
+from flask_restful_swagger import swagger
 
-class HelloWorld(Resource):
+class Bootstrap(Resource):
+    @swagger.operation(
+        notes='a test note',
+        nickname='bootstrap',
+        responseMessages=[
+            {
+              "code": 200,
+              "message": "list node information"
+            }
+          ]
+        )
     def get(self):
-        return {'echo': 'hello world'}
+        return {'echo': 'list docker image ids and container ids'}
+
+    @swagger.operation(
+        notes='a test note',
+        nickname='bootstrap',
+        responseMessages=[
+            {
+              "code": 200,
+              "message": "node created"
+            }
+          ]
+        )
+    def post(self):
+        return {'echo': 'list newly created docker image ids and container ids'}
+
+    @swagger.operation(
+        notes='a test note',
+        nickname='bootstrap',
+        responseMessages=[
+            {
+              "code": 200,
+              "message": "node created"
+            }
+          ]
+        )
+    def delete(self):
+        return {'echo': 'list deleted docker image ids and container ids'}
--- run.py
+++ run.py
@@ -8,9 +8,10 @@ from api.app import create_app
 from api.settings import DevConfig, ProdConfig
 #from api.extensions import restapi
 from flask.ext import restful
+from flask_restful_swagger import swagger
 
 #import resources
-from api.resources import HelloWorld
+from api.resources import Bootstrap
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
@@ -19,10 +20,11 @@ if os.environ.get("API_ENV") == 'prod':
 else:
     app = create_app(DevConfig)
 
-restapi = restful.Api()
+#restapi = restful.Api()
+restapi = swagger.docs(restful.Api(app), apiVersion='0.1')
 #restapi.init_app(app)
-restapi.add_resource(HelloWorld, '/bootstrap')
-restapi.init_app(app)
+restapi.add_resource(Bootstrap, '/bootstrap')
+#restapi.init_app(app)
 
 if __name__ == '__main__':
     app.run(port = app.config['PORT'])
