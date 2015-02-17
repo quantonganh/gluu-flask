94dba767fc2cda4d23b613b2b720ffffd5994cf9
Isman Firmansyah <isman.firmansyah@gmail.com>
2015-02-17 11:10:25 +0700

    create database root directory if not exist

--- api/database.py
+++ api/database.py
@@ -22,6 +22,7 @@
 
 import inspect
 import jsonpickle
+import os
 import tinydb
 
 
@@ -53,6 +54,10 @@ class Database(object):
                          "application. Ensure you have called init_app first."
 
         if not self._db:
+            try:
+                os.makedirs(os.path.dirname(self.app.config["DATABASE_URI"]))
+            except OSError:
+                pass
             self._db = tinydb.TinyDB(self.app.config["DATABASE_URI"])
         return self._db
 
