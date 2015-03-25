7ef82e7235bb9307d31d6fedcda1fc2cf3065c46
Isman Firmansyah <isman.firmansyah@gmail.com>
2015-03-26 03:46:20 +0700

    hide setup-related attributes in ldapNode when rendering JSON response

--- api/model/ldap_node.py
+++ api/model/ldap_node.py
@@ -39,37 +39,11 @@ class ldapNode(BaseModel):
         'cluster_id': fields.String(attribute='Cluster ID'),
         'local_hostname': fields.String(attribute='Local hostname of the node (not the cluster hostname).'),
         'ip': fields.String(attribute='IP address of the node'),
-        'ldap_setup_properties': fields.String(attribute='Filesystem path of the opendj-setup.properties template'),
-        'openDjCertFn': fields.String(attribute='Filesystem path of the public certificate for OpenDJ'),
         'ldap_binddn': fields.String(attribute='LDAP super user Bind DN. Probably should leave it default cn=directory manager.'),
         'ldap_port': fields.String(attribute='Non SSL LDAP port (not used)'),
         'ldaps_port': fields.String(attribute='LDAPS port'),
         'ldap_admin_port': fields.String(attribute='Admin port'),
         'ldap_jmx_port': fields.String(attribute='JMX port (not used)'),
-        'ldapBaseFolder': fields.String(attribute='Where to install OpenDJ, usually /opt/opendj'),
-        'ldapStartTimeOut': fields.String(attribute='How long to wait for LDAP to start.'),
-        'ldapSetupCommand': fields.String(attribute='Full path to opendj setup command'),
-        'ldapDsconfigCommand': fields.String(attribute='Full path to dsconfig command'),
-        'ldapDsCreateRcCommand': fields.String(attribute='Full path to create-rc command'),
-        'ldapDsJavaPropCommand': fields.String(attribute='Full path to dsjavaproperties command'),
-        'importLdifCommand': fields.String(attribute='Full path to import command'),
-        'ldapEncodePWCommand': fields.String(attribute='Full path to encode password'),
-        'ldapPassFn': fields.String(attribute='Temporary path to store ldap password (should be removed)'),
-        'schemaFolder': fields.String(attribute='Full path of template schema to copy to the opendj server'),
-        'init_file': fields.String(attribute='Full path of tempalte init file'),
-        'ldap_start_script': fields.String(attribute='Full path of the destination of the init script'),
-        'keytoolCommand': fields.String(attribute='Full path to java keytool command'),
-        'opensslCommand': fields.String(attribute='Full path to openssl command'),
-        'ldif_base': fields.String(attribute='Full path to output folder ldif'),
-        'ldif_appliance': fields.String(attribute='Full path to output folder ldif'),
-        'ldif_attributes': fields.String(attribute='Full path to output folder ldif'),
-        'ldif_scopes': fields.String(attribute='Full path to output folder ldif'),
-        'ldif_clients': fields.String(attribute='Full path to output folder ldif'),
-        'ldif_people': fields.String(attribute='Full path to output folder ldif'),
-        'ldif_groups': fields.String(attribute='Full path to output folder ldif'),
-        'ldif_site': fields.String(attribute='Full path to output folder ldif'),
-        'ldif_scripts': fields.String(attribute='Full path to output folder ldif'),
-        'ldif_files': fields.List(fields.String, attribute='List of initial ldif files'),
     }
 
     def __init__(self):
@@ -78,23 +52,47 @@ class ldapNode(BaseModel):
 
         self.local_hostname = ""
         self.ip = ""
-        self.ldap_setup_properties = "api/templates/salt/opendj" \
-                                     "/opendj-setup.properties"
+
+        # Filesystem path of the opendj-setup.properties template
+        self.ldap_setup_properties = "api/templates/salt/opendj/opendj-setup.properties"
+
+        # Filesystem path of the public certificate for OpenDJ
         self.openDjCertFn = '/etc/certs/opendj.crt'
+
         self.ldap_binddn = 'cn=directory manager'
         self.ldap_port = '1389'
         self.ldaps_port = '1636'
         self.ldap_jmx_port = '1689'
         self.ldap_admin_port = '4444'
+
+        # Where to install OpenDJ, usually /opt/opendj
         self.ldapBaseFolder = '/opt/opendj'
+
+        # How long to wait for LDAP to start
         self.ldapStartTimeOut = 30
+
+        # Full path to opendj setup command
         self.ldapSetupCommand = '%s/setup' % self.ldapBaseFolder
+
+        # Full path to dsconfig command
         self.ldapDsconfigCommand = "%s/bin/dsconfig" % self.ldapBaseFolder
+
+        # Full path to create-rc command
         self.ldapDsCreateRcCommand = "%s/bin/create-rc-script" % self.ldapBaseFolder
+
+        # Full path to dsjavaproperties command
         self.ldapDsJavaPropCommand = "%s/bin/dsjavaproperties" % self.ldapBaseFolder
+
+        # Full path to import command
         self.importLdifCommand = '%s/bin/import-ldif' % self.ldapBaseFolder
+
+        # Full path to encode password
         self.ldapEncodePWCommand = '%s/bin/encode-password' % self.ldapBaseFolder
+
+        # Temporary path to store ldap password (should be removed)
         self.ldapPassFn = '/home/ldap/.pw'
+
+        # Full path of template schema to copy to the opendj server
         self.schemaFolder = "%s/template/config/schema" % self.ldapBaseFolder
         self.org_custom_schema = "%s/config/schema/100-user.ldif" % self.ldapBaseFolder
         self.schemaFiles = [
@@ -103,14 +101,22 @@ class ldapNode(BaseModel):
             "api/templates/salt/opendj/schema/96-eduperson.ldif",
             "api/templates/salt/opendj/schema/100-user.ldif",
         ]
+
+        # Full path of template init file
         self.init_file = '%s/static/opendj/opendj' % self.install_dir
+
+        # Full path of the destination of the init script
         self.ldap_start_script = '/etc/init.d/opendj'
 
-        # self.keytoolCommand = '/usr/java/latest/bin/keytool'
+        # Full path to java keytool command
         self.keytoolCommand = '/usr/bin/keytool'
+        # self.keytoolCommand = '/usr/java/latest/bin/keytool'
+
+        # Full path to openssl command
         self.opensslCommand = '/usr/bin/openssl'
 
         self.outputFolder = '/tmp'
+
         self.ldif_base = 'api/templates/salt/opendj/ldif/base.ldif'
         self.ldif_appliance = 'api/templates/salt/opendj/ldif/appliance.ldif'
         self.ldif_attributes = 'api/templates/salt/opendj/ldif/attributes.ldif'
@@ -120,6 +126,8 @@ class ldapNode(BaseModel):
         self.ldif_groups = 'api/templates/salt/opendj/ldif/groups.ldif'
         self.ldif_site = 'api/templates/salt/opendj/ldif/o_site.ldif'
         self.ldif_scripts = 'api/templates/salt/opendj/ldif/scripts.ldif'
+
+        # List of initial ldif files
         self.ldif_files = [
             self.ldif_base,
             self.ldif_appliance,
